7e1b91c4b77861d2ed9cd97951ac18152009c2b6
zhj20 <99gelanjingling@gmail.com>
2024-07-11 19:30:10 +0800

    启动类忘了加MapperScan

--- springCloud-provider-8006/src/main/java/springCloud/Provider8006Application.java
+++ springCloud-provider-8006/src/main/java/springCloud/Provider8006Application.java
@@ -1,5 +1,6 @@
 package springCloud;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient //开启 Eureka 客户端功能
+@MapperScan("springCloud.mapper")
 public class Provider8006Application {
     public static void main(String[] args) {
         SpringApplication.run(Provider8006Application.class, args);
